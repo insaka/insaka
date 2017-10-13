733d86c202d21f79fa0c370f83653ec1c0ed9b23
insaka <admin@insaka.org>
2017-10-12 23:08:16 -0400

    Minor typo and picture edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002001732) do
+ActiveRecord::Schema.define(version: 20171012034112) do
+
+  create_table "girls", force: :cascade do |t|
+    t.string "name"
+    t.float "percent_funded"
+    t.datetime "began_funding"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
