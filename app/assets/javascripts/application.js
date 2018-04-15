7f90e47422bb628fbfaf5101e6b3fda95ae3fbfe
d-m-u <drewuhlmann@gmail.com>
2018-04-15 08:29:49 -0400

    Update gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require popper
+//= require turbolinks
 //= require jquery3
 //= require rails-ujs
 //= require bootstrap-sprockets
 //= require_tree .
-//= require popper
