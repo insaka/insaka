80ae01240cadb477b4e058094e0c47034b92663a
wabray17 <wabray17@gmail.com>
2019-03-31 15:33:03 -0500

    Added views and controller methods for edit/update

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -7,4 +7,21 @@ class GirlsController < ApplicationController
   def show
     @girl = Girl.find(params[:id])
   end
+
+  def edit 
+    @girl = Girl.find(params[:id])
+  end
+
+  def update
+    @girl = Girl.find(params[:id])
+
+    @girl.update(first_name: params[:first_name],
+                 last_name: params[:last_name],
+                 description: params[:description],
+                 quote: params[:quote],
+                 status: params[:status],
+                 image: params[:image])
+
+    redirect_to "/girls/#{@girl.id}"
+  end
 end
