b133827029dbbd3fda14c2138105fd35bcdabd0d
wabray17 <wabray17@gmail.com>
2019-11-10 13:46:54 -0600

    Add mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,10 +62,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
+   :address              => "smtp.mailgun.org",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :domain               => ENV['mg_domain'],
+   :user_name            => ENV['mg_user'],
+   :password             => ENV['mg_pass'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
